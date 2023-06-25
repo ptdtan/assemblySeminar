336bfe1eb60de0545f0f5cf79d0fffda7b835b90
Tan Phan <ptdtan@gmail.com>
2023-06-25 18:27:10 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,6 @@ wget -O SRR24112717_R1.fastq https://www.dropbox.com/s/jbzcqr8r3qybtk8/SRR241127
 
 # Run unicycler, assuming that your user can run docker. This command will create the output folder sars-cov2-assembly in your working directory
 docker run -it  -v $PWD:/data staphb/unicycler unicycler -1 SRR24112717_R1.fastq -2 SRR24112717_R2.fastq -o sars-cov2-assembly --kmers 13,25 --threads 10
+
+# Run Quast to assess to genome quality
+docker run -it -v $PWD:/data staphb/quast quast.py sars-cov2-assembly/assembly.fasta
