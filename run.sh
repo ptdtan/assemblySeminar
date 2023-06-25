f775337708dca5e3a704d7af482b09ccb824029d
Tan Phan <ptdtan@gmail.com>
2023-06-25 18:07:30 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+# download input files
+wget -O SRR24112717_R2.fastq https://www.dropbox.com/s/euu28tbzoj29k4p/SRR24112717_R2.fastq?dl=1
+wget -O SRR24112717_R1.fastq https://www.dropbox.com/s/jbzcqr8r3qybtk8/SRR24112717_R1.fastq?dl=1
+
+# Run unicycler, assuming that your user can run docker. This command will create the output folder sars-cov2-assembly in your working directory
+docker run -it  -v $PWD:/data staphb/unicycler unicycler -1 SRR24112717_R1.fastq -2 SRR24112717_R2.fastq -o sars-cov2-assembly --kmers 13,25 --threads 10
